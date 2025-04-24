8d62f8a36fea91abe5e81f9a8fa15dd9a43ab3b6
Rifki779 <rifkisuryanahasbi@gmail.com>
2025-04-24 11:17:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("LATIHAN MEMBUAT WEB MYLOVEDEZAN🥰")
+st.title("LATIHAN MEMBUAT WEB MYLOVEDEZAN")
 st.write(
-    "website punya Rifki Suryana X-B SMAN 20 BANDUNG
+    "website punya Rifki Suryana X-B SMAN 20 BANDUNG")
